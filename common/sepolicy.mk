f7887e0bd06657084d6e18e99d0129263ed48780
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-18 18:21:00 +0530

    {common,qcom}:sepolicy.mk: update for RR
    Change-Id: Ibececd0a7d510143ce018fb30ed39623882b2c74
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from RR
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/rr/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/rr/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/rr/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from RR
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/rr/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/rr/sepolicy/qcom/common \
+    device/rr/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
